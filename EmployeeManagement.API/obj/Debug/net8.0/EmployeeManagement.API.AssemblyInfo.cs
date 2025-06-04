ac1789067c0f0f3a82efeda0ff2f56fda8d3b9a0
Mohammed Amaan <amaanvisiontech@gmail.com>
2025-06-04 12:35:04 +0530

    API build

--- EmployeeManagement.API/obj/Debug/net8.0/EmployeeManagement.API.AssemblyInfo.cs
+++ EmployeeManagement.API/obj/Debug/net8.0/EmployeeManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6fa152df2b661df0836fcb047f0778bd88e799")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e0584b608c0935d73fdeab1c3aeb853a5451a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
